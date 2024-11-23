3ffa09d6a087d9908e2bc733820fae9c1dc7e7b7
MickeyOnyando <otienomichael360@gmail.com>
2024-11-23 19:27:01 +0300

    "Refactored main.dart, removed MyHomePage and MyHomePageState, added SplashPage, updated pubspec.yaml and pubspec.lock with new dependencies"

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+
+  const SplashPage({super.key, required this.onInitializationComplete});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
